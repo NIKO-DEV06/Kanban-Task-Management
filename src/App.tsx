24abe9786ca15904e87553a4bee39ae220099900
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-05-04 08:16:45 +0100

    added file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className="text-blue-800">Kanban</h1>
+      <h1 className="text-blue-800 text-8xl">Kanban</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "input-shadow": "0 1px 15px 1px rgba(0, 0, 0, 0.1)",
+        "header-shadow": "0 2px 8px 0px rgba(0, 0, 0, 0.1)",
+      },
+
+      fontFamily: {
+        "plus-jakarta-sans": ['"Plus Jakarta Sans"', "sans-serif"],
+      },
+      letterSpacing: {
+        widest: ".3em",
+      },
+      dropShadow: {
+        glow: "0 0 1em hsl(26, 100%, 55%)",
+      },
+    },
   },
+
   plugins: [],
 };
